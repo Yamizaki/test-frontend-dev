e65a0252e649e461e646461ea677ece23c33a0af
4shakes <jayro1996.ag@gmail.com>
2025-03-16 21:18:09 -0500

    Add validation and error handling to login form with Zod and React Hook Form

--- test-cursos/src/modules/auth/pages/login-page.jsx
+++ test-cursos/src/modules/auth/pages/login-page.jsx
@@ -5,30 +5,50 @@ import axios from "axios";
 import { authStore } from "../store/auth-store";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../../core/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Bounce, toast } from 'react-toastify';
 
+const LoginSchema = z.object({
+  username: z.string().nonempty('El campo es obligatorio'),
+  password: z.string().nonempty('El campo es obligatorio'),
+});
 
 export const LoginPage = () => {
-  const { register, handleSubmit } = useForm();
-  const { setUserToken } = authStore();
+  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm({ resolver: zodResolver(LoginSchema) })
+  console.log(errors);
+
+  const { setUserToken } = authStore()
   const navigate = useNavigate()
 
 
   const handleLogin = async (data) => {
-    console.log(data);
+
     try {
       // se agrega el prefijo /api para que funcione el proxy de vite
-      const response = await axios.post("/api/login", data);
+      const response = await axios.post("/api/login", data)
       if (response.status === 200) {
-        setUserToken(response.data.access_token);
-        navigate("/");
+        setUserToken(response.data.access_token)
+        navigate("/")
       }
     } catch (error) {
       console.log(error);
+      toast.error(error.response.data.msg, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      })
     }
 
 
 
-  };
+  }
 
   return (
     <div className="flex h-screen">
@@ -62,7 +82,10 @@ export const LoginPage = () => {
                 {...register("username")}
                 className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
               />
+              {errors && <small className="text-sm">{errors.username?.message}</small>}
             </div>
+
+
             <div>
               <label
                 for="password"
@@ -77,13 +100,14 @@ export const LoginPage = () => {
                 {...register("password")}
                 className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
               />
+              {errors && <small className="text-sm">{errors.password?.message}</small>}
             </div>
             <div>
-              <Button>Iniciar sesión</Button>
+              <Button disabled={isSubmitting} isLoading={isSubmitting}>Ingresar</Button>
             </div>
           </form>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
